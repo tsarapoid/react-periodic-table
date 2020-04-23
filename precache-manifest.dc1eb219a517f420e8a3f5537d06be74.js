251cb2f7f9460676bbdfe55210f8a0f19339749d
tsarapoid <nadein@gmail.com>
2020-04-23 19:20:21 +0300

    Updates

---
+++ precache-manifest.dc1eb219a517f420e8a3f5537d06be74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77605e6e31defeb0b424245286ca620c",
+    "url": "/react-periodic-table/index.html"
+  },
+  {
+    "revision": "40d3dd1ac67c9ff4c374",
+    "url": "/react-periodic-table/static/css/main.0d13592b.chunk.css"
+  },
+  {
+    "revision": "844c9750a8ef346b3ccf",
+    "url": "/react-periodic-table/static/js/2.290a4f52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-periodic-table/static/js/2.290a4f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d3dd1ac67c9ff4c374",
+    "url": "/react-periodic-table/static/js/main.4bc7972c.chunk.js"
+  },
+  {
+    "revision": "6fb285892edd9ff0820a",
+    "url": "/react-periodic-table/static/js/runtime-main.7cfd7aba.js"
+  }
+]);
